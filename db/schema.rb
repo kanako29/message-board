50b114529f4938d8fd101f97f70c0e97faba9acc
Kanako Sakamoto <kanako0729@gmail.com>
2016-01-02 05:08:33 +0000

    create form of age

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151229032406) do
+ActiveRecord::Schema.define(version: 20160102040306) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
